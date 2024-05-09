384815a02ff8b6889d7ee536ceb9cbab8738fac7
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2024-05-09 13:11:22 +0200

    Corregido fallo con groupby de tags y autores

--- src/main/java/com/cca/ia/rag/prompt/model/DashboardStatsRepository.java
+++ src/main/java/com/cca/ia/rag/prompt/model/DashboardStatsRepository.java
@@ -18,10 +18,10 @@ public interface DashboardStatsRepository extends Repository<PromptEntity, Long>
             + "select null as id, 'last_views' as label, count(1) as value from prompt_view p where (p.date >= cast((now() - interval 1 month) as date))", nativeQuery = true)
     List<DashboardStatsDto.ChartData> getGlobalStats();
 
-    @Query(value = "select id as id, tag as label, count(1) as value from prompt_tag group by id, tag order by count(1) desc limit 10", nativeQuery = true)
+    @Query(value = "select min(id) as id, tag as label, count(1) as value from prompt_tag group by tag order by count(1) desc limit 10", nativeQuery = true)
     List<DashboardStatsDto.ChartData> getTopTags();
 
-    @Query(value = "select ap.id as id, concat(ap.name, ' ', ap.lastname) as label, count(1) as value from prompt p join personal.all_person ap on p.person_id = ap.id group by ap.id, concat(ap.name, ' ', ap.lastname) order by count(1) desc limit 10", nativeQuery = true)
+    @Query(value = "select min(ap.id) as id, concat(ap.name, ' ', ap.lastname) as label, count(1) as value from prompt p join personal.all_person ap on p.person_id = ap.id group by concat(ap.name, ' ', ap.lastname) order by count(1) desc limit 10", nativeQuery = true)
     List<DashboardStatsDto.ChartData> getTopPersons();
 
     @Query(value = "select p.id as id, p.title as label, count(1) as value from prompt_view pv join prompt p on pv.prompt_id = p.id group by p.id, p.title order by count(1) desc limit 10", nativeQuery = true)
